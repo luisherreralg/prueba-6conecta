d3b0dd3c7391b6a1b0a7d68393120227fb091cba
luisherreralg <lherreralg@outlook.es>
2023-01-21 14:27:33 +0100

    Added header module, some logic and styles

--- src/app/header/header-filters/header-filters.component.ts
+++ src/app/header/header-filters/header-filters.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { ApiServiceService } from 'src/app/services/api.service.service';
+import { Pokemon } from 'src/app/utils/interfaces/PokemonByType';
 
 @Component({
   selector: 'app-header-filters',
   templateUrl: './header-filters.component.html',
   styleUrls: ['./header-filters.component.sass'],
 })
-export class HeaderFiltersComponent {}
+export class HeaderFiltersComponent {
+  form: FormGroup;
+
+  constructor(public apiService: ApiServiceService, public store: Store) {
+    this.form = new FormGroup({
+      type: new FormControl(),
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onChange = (event: any): void => {
+    this.apiService
+      .getPokemonByTypeList(event.target.value)
+      .subscribe((data): void => {
+        data.pokemon.forEach((pokemon: Pokemon): void => {
+          this.apiService.getPokemon(pokemon.pokemon.name);
+        });
+      });
+  };
+}
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { HeaderTitleComponent } from './header-title/header-title.component';
 import { HeaderFiltersComponent } from './header-filters/header-filters.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HeaderComponent, HeaderTitleComponent, HeaderFiltersComponent],
-  imports: [CommonModule],
+  imports: [ReactiveFormsModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
