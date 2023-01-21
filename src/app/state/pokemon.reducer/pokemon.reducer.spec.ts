37c49ff202c2294a9214e342f3fcde1c55fc9b8b
luisherreralg <lherreralg@outlook.es>
2023-01-21 18:16:47 +0100

    Finished the test of the pokemon.reducer

--- src/app/state/pokemon.reducer/pokemon.reducer.spec.ts
+++ src/app/state/pokemon.reducer/pokemon.reducer.spec.ts
@@ -10,7 +10,17 @@ describe('Given the PokemonReducer', (): void => {
   const pokemonMock: Pokemon[] = [{} as Pokemon];
 
   const initialState = {
-    pokemons: [] as Pokemon[],
+    pokemons: [
+      {
+        id: 1,
+        name: 'bulbasaur',
+      },
+
+      {
+        id: 2,
+        name: 'ivysaur',
+      },
+    ] as Pokemon[],
   };
 
   describe('Given the loadOrders action', (): void => {
@@ -24,43 +34,49 @@ describe('Given the PokemonReducer', (): void => {
 
   describe('Given the addOrder action', (): void => {
     it('should add a new order', (): void => {
-      pokemonMock[0].id = 1;
-
       const action = addPokemon({ newPokemon: pokemonMock[0] });
       const state = PokemonReducer(initialState, action);
 
-      expect(state.pokemons.length).toBe(1);
+      expect(state.pokemons.length).toBe(3);
     });
   });
 
   describe('Given the deleteOrder action', (): void => {
     it('should delete an existing order', (): void => {
-      pokemonMock[0].id = 1;
-
-      const action = deletePokemon({ pokemonId: pokemonMock[0].id });
+      const action = deletePokemon({ pokemonId: 1 });
       const state = PokemonReducer(initialState, action);
 
-      expect(state.pokemons.length).toBe(0);
+      expect(state.pokemons.length).toBe(1);
     });
 
     it('should not delete an order if the id does not exist', (): void => {
-      pokemonMock[0].id = 1;
-
       const action = deletePokemon({ pokemonId: 2 });
       const state = PokemonReducer(initialState, action);
 
-      expect(state.pokemons.length).toBe(0);
+      expect(state.pokemons.length).toBe(1);
     });
 
     it('the filter inside the delete pokemon action, should return false if there is not matching ids', (): void => {
       const spyFilter = spyOn(Array.prototype, 'filter').and.callThrough();
 
-      pokemonMock[0].id = 1;
-
       const action = deletePokemon({ pokemonId: 2 });
       PokemonReducer(initialState, action);
 
       expect(spyFilter).toHaveBeenCalledWith(jasmine.any(Function));
+    });
+
+    it('should not return any pokemon if the id does not exist', (): void => {
+      const action = deletePokemon({ pokemonId: 2 });
+      const state = PokemonReducer(initialState, action);
+
+      expect(state.pokemons.length).toBe(1);
+    });
+
+    it('should return the pokemon if the id exists', (): void => {
+      const action = deletePokemon({ pokemonId: 1 });
+      const state = PokemonReducer(initialState, action);
+
+      expect(state.pokemons.length).toBe(1);
     });
   });
 });
