926bb297a7c5d1fb4c0535ecd8a76f499e0a128e
luisherreralg <lherreralg@outlook.es>
2023-01-21 17:36:15 +0100

    Fixed some lint and style errors

--- src/app/header/header-title/header-title.component.spec.ts
+++ src/app/header/header-title/header-title.component.spec.ts
@@ -2,22 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderTitleComponent } from './header-title.component';
 
-describe('HeaderTitleComponent', () => {
+describe('HeaderTitleComponent', (): void => {
   let component: HeaderTitleComponent;
   let fixture: ComponentFixture<HeaderTitleComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async (): Promise<void> => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderTitleComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderTitleComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderTitleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', (): void => {
     expect(component).toBeTruthy();
   });
 });
