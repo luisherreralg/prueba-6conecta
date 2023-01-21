de56e1b8bf3c28888c0cd3f9029590b8bad27674
luisherreralg <lherreralg@outlook.es>
2023-01-21 19:22:50 +0100

    Finished the add-form test and updated other ones

--- src/app/add-form/add-form.component.spec.ts
+++ src/app/add-form/add-form.component.spec.ts
@@ -2,22 +2,46 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddFormComponent } from './add-form.component';
 
-describe('AddFormComponent', () => {
+describe('AddFormComponent', (): void => {
   let component: AddFormComponent;
   let fixture: ComponentFixture<AddFormComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async (): Promise<void> => {
     await TestBed.configureTestingModule({
-      declarations: [ AddFormComponent ]
-    })
-    .compileComponents();
+      declarations: [AddFormComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', (): void => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Given the addPokemon method, when its invoked', (): void => {
+    it('should call to the pokemonService addPokemon method, form reset and toggleButton method', (): void => {
+      const spyAddPokemon = spyOn(component.pokemonService, 'addPokemon');
+      const spyReset = spyOn(component.form, 'reset');
+      const spyToggle = spyOn(component, 'toggleButton');
+
+      spyOn(component.form, 'value').and.returnValue({});
+
+      component.addPokemon();
+
+      // eslint-disable-next-line jasmine/prefer-toHaveBeenCalledWith
+      expect(spyAddPokemon).toHaveBeenCalled();
+      expect(spyReset).toHaveBeenCalledWith();
+      expect(spyToggle).toHaveBeenCalledWith();
+    });
+  });
+
+  describe('Given the toggleButton method, when its invoked', (): void => {
+    it('should change the toggle variable value', (): void => {
+      component.toggleButton();
+
+      expect(component.toggle).toBe(true);
+    });
   });
 });
