43c7f1f45780f1a97ef1890b29db5b62fea56b98
luisherreralg <lherreralg@outlook.es>
2023-01-21 16:03:18 +0100

    Removed Store from ngrx and updated styles & html

--- src/app/header/header-filters/header-filters.component.ts
+++ src/app/header/header-filters/header-filters.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
 import { ApiServiceService } from 'src/app/services/api.service.service';
+import { PokemonsService } from 'src/app/services/pokemons.service';
 import { Pokemon } from 'src/app/utils/interfaces/PokemonByType';
+import { Generation } from 'src/app/utils/interfaces/PokemonByType';
+import { Result } from 'src/app/utils/interfaces/PokemonList';
 
 @Component({
   selector: 'app-header-filters',
@@ -12,19 +15,58 @@ import { Pokemon } from 'src/app/utils/interfaces/PokemonByType';
 export class HeaderFiltersComponent {
   form: FormGroup;
 
-  constructor(public apiService: ApiServiceService, public store: Store) {
+  pokeUrls: Generation[] = [];
+
+  pokeUrls$: Subject<Generation[]>;
+
+  constructor(
+    public apiService: ApiServiceService,
+    public pokemonService: PokemonsService
+  ) {
     this.form = new FormGroup({
       type: new FormControl(),
+      order: new FormControl(),
     });
+    this.pokeUrls$ = new Subject();
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onChange = (event: any): void => {
+    if (event.target.value === 'asc' || event.target.value === 'desc') {
+      return this.pokemonService.orderPokemons(event.target.value);
+    }
+
+    this.pokemonService.resetPokemons();
+
+    if (event.target.value === 'all') {
+      this.resetToDefault();
+    }
+
+    this.loadPokemonByType(event);
+  };
+
+  resetToDefault = (): void => {
+    this.apiService.getPokemonList().subscribe((data): void => {
+      data.results.forEach((pokemon: Result): void => {
+        this.apiService.getPokemon(pokemon.name).subscribe((data2): void => {
+          this.pokemonService.addPokemon(data2);
+        });
+      });
+    });
+    return;
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  loadPokemonByType = (event: any): void => {
     this.apiService
       .getPokemonByTypeList(event.target.value)
       .subscribe((data): void => {
         data.pokemon.forEach((pokemon: Pokemon): void => {
-          this.apiService.getPokemon(pokemon.pokemon.name);
+          this.apiService
+            .getPokemon(pokemon.pokemon.name)
+            .subscribe((data2): void => {
+              this.pokemonService.addPokemon(data2);
+            });
         });
       });
   };
--- src/app/pokemon-table/pokemon-table.component.ts
+++ src/app/pokemon-table/pokemon-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ApiServiceService } from '../services/api.service.service';
+import { PokemonsService } from '../services/pokemons.service';
 import { Pokemon } from '../utils/interfaces/Pokemon';
 import { Result } from '../utils/interfaces/TypeList';
 
@@ -16,11 +17,18 @@ export class PokemonTableComponent implements OnInit {
 
   pokemons: Pokemon[] = [];
 
-  constructor(public apiService: ApiServiceService) {
+  constructor(
+    public apiService: ApiServiceService,
+    public pokemonService: PokemonsService
+  ) {
     this.pokeUrls$ = new Subject();
   }
 
   ngOnInit(): void {
+    this.pokemonService.getPokemons().subscribe((data): void => {
+      this.pokemons = data;
+    });
+
     this.apiService.getPokemonList().subscribe((data): void => {
       this.pokeUrls = data.results;
       this.pokeUrls$.next(this.pokeUrls);
@@ -29,7 +37,7 @@ export class PokemonTableComponent implements OnInit {
     this.pokeUrls$.subscribe((data): void => {
       data.forEach((url): void => {
         this.apiService.getPokemon(url.name).subscribe((pokemon): void => {
-          this.pokemons.push(pokemon);
+          this.pokemonService.addPokemon(pokemon);
         });
       });
     });
