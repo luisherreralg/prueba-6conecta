0cc1f2ad2bbd48ad505fdcf9a00fd2fd7fc3730f
luisherreralg <lherreralg@outlook.es>
2023-01-21 20:34:06 +0100

    Updated the project to a pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,7 @@ import { HeaderModule } from './header/header.module';
 import { PokemonTableModule } from './pokemon-table/pokemon-table.module';
 import { AddFormComponent } from './add-form/add-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, AddFormComponent],
@@ -19,6 +20,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     HeaderModule,
     PokemonTableModule,
     ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
